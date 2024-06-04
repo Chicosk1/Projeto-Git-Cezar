92c51863bfc14c530c2dd20b7fdb55a9787c5413
Chicosk1 <gabrielsamudiocichocki@gmail.com>
2024-06-03 21:25:44 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String continuar = "SIM";
+        boolean validação = true;
+        do{
+        double a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe a operação que você deseja realizar:");
+        System.out.println("1 - Soma                2 - Subtração");
+        System.out.println("3 - Multiplicação       4 - Divisão");
+        int op = sc.nextInt();
+            if (op >= 1 && op <= 4) {
+                System.out.println("Informe o valor de 'a': ");
+                a = sc.nextDouble();
+                System.out.println("Informe o valor de 'b': ");
+                b = sc.nextDouble();
+            }
+        }while(continuar.equalsIgnoreCase("SIM"));
+    
+    }
+}
